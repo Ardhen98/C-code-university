f9d6e2c4674c91f796b94bd317e197871962c0f9
Ardhen98 <ardhenbarge@gmail.com>
2017-10-11 22:20:46 -0400

    Update and rename more of c to more of c.c

---
+++ new folder/more of c.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int a, b;
+  printf("Enter number a: ");
+  scanf("%d",a);
+  printf("Enter number b: ");
+  scanf("%d",b);
+  printf("Addition of a and b is:%d\n", a + b);
+}

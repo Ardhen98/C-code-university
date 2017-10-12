346015de9ac18855538c01e41463849fcd0f4e1c
Ardhen98 <ardhenbarge@gmail.com>
2017-10-11 22:13:31 -0400

    Update add.c

--- add.c
+++ add.c
@@ -2,7 +2,7 @@
 
 int main(void)
 {
-  int a = 2;
-  int b = 4;
-  printf("Add a and b is:%d\n", a+b);
+  int a = 2; // value of a is 2
+  int b = 4; // value of b is 4
+  printf("Add a and b is:%d\n", a+b); //prints out the addition of a and b
 }

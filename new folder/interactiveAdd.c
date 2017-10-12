1bd04e4704d1b581875830fa7e75ea072adf00fb
Ardhen98 <ardhenbarge@gmail.com>
2017-10-11 22:23:43 -0400

    Update and rename more of c.c to interactiveAdd.c

--- new folder/interactiveAdd.c
+++ new folder/interactiveAdd.c
@@ -4,8 +4,8 @@ int main(void)
 {
   int a, b;
   printf("Enter number a: ");
-  scanf("%d",a);
+  scanf("%d",&a);
   printf("Enter number b: ");
-  scanf("%d",b);
+  scanf("%d",*b);
   printf("Addition of a and b is:%d\n", a + b);
 }

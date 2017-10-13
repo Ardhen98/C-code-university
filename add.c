9c93f03b5dae217c74e6705eedf41bee039c014e
Ardhendu Barge <ardhenbarge@gmail.com>
2017-10-13 17:33:11 -0400

    Delete add.c

--- add.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-
-int main(void)
-{
-  int a = 2; // value of a is 2
-  int b = 4; // value of b is 4
-  printf("Add a and b is:%d\n", a+b); //prints out the addition of a and b
-}
